4fd2f216c972b9a789f4123d2a4436e5e48ddd91
Kent Gulka <kentgulka@modindsoft.com>
2022-03-22 16:55:15 -0600

    changes required to debug server, storybook and jest installs

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ["../stories/index.js"],
+  addons: [
+    "@storybook/preset-scss",
+    "@storybook/addon-actions/register",
+    "@storybook/addon-backgrounds/register",
+  ],
+};
