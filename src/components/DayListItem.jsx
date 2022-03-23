1318fc68c38f1b68e91ba10c3b8aac83d7f75584
Kent Gulka <kentgulka@modindsoft.com>
2022-03-23 14:35:36 -0600

    added conditional styles and prop logic to dayListItem

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -1,9 +1,19 @@
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
 export default function DayListItem(props) {
+  let listItemClass = classNames("day-list__item", {"day-list__item--selected": props.selected}, {"day-list__item--full": props.spots === 0} );
+  
+  console.log(listItemClass);
+
+  const formatSpots = function(spotsIn) {
+    return spotsIn === 0 ? "no spots remaining" : spotsIn === 1 ? "1 spot remaining" : spotsIn + " spots remaining";
+  }
 
   return (
-    <li className={props.selected} onClick={() => props.setDay(props.name)}>
+    <li className={listItemClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
