1830ac4e1b44606050ff6e6f35815e0d65fdec2c
Kent Gulka <kentgulka@modindsoft.com>
2022-03-22 23:03:56 -0600

    updated button component and started daylistitem

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 import "components/Button.scss";
+import classNames from "classnames";
+
 
 export default function Button(props) {
-   return <></>;
+  let clickHandler = "";
+  let buttonClass = classNames("button ",{ " button--confirm": props.confirm}, {" button--danger": props.danger} );
+
+  // if (props.confirm) {
+  //   buttonClass += " button--confirm";
+  // }
+  // if (props.danger) {
+  //   buttonClass += " button--danger";
+  // }
+  
+  return (
+    <button 
+      onClick={props.onClick} 
+      className={buttonClass} 
+      disabled={props.disabled}
+    >
+        {props.children}
+    </button>
+  );
 }
---
+++ src/components/DayListItem.jsx
@@ -0,0 +1,9 @@
+export default function DayListItem(props) {
+
+  return (
+    <li className={props.selected} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{props.spots} spots remaining</h3>
+    </li>
+  );
+}
