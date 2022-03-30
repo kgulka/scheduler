1f81eb4f2950ca4e13d243436dbc79dc3a97becf
Kent Gulka <kentgulka@modindsoft.com>
2022-03-30 15:05:56 -0600

    implemented React unit tests on the Form.jsx component

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -1,18 +1,29 @@
-import { useState } from "react";
+
+import React, { useState } from "react";
 import Button from "../Button";
 import InterviewerList from "../InterviewerList";
 
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null,[]);
+  const [error, setError] = useState("");
 
   const reset = function() {
     setStudent("");
     setInterviewer(null);
   }
   const cancel = function() {
+    setError("");
     reset();
     props.onCancel();
+  }
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    setError("");
+    props.onSave(student, interviewer);
   }
   return (
     <main className="appointment__card appointment__card--create">
@@ -25,8 +36,10 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => setStudent(event.target.value)}
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList 
           interviewers={props.interviewers}
           onChange={setInterviewer}
@@ -36,9 +49,9 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button onClick={cancel} danger>Cancel</Button>
-          <Button onClick={() => props.onSave(student, interviewer )} confirm >Save</Button>
+          <Button onClick={() => validate()} confirm >Save</Button>
         </section>
       </section>
     </main>
-  );
+  )
 }
